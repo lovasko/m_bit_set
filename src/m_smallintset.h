b951091a6c4c0999981aeb6456d8398a3ba1ed69
Daniel Lovasko <lovasko@freebsd.org>
2016-01-25 15:22:27 +0100

    prototypes on one line

--- src/m_smallintset.h
+++ src/m_smallintset.h
@@ -13,22 +13,16 @@ struct m_smallintset {
 };
 
 int m_smallintset_init(struct m_smallintset* sis);
-int m_smallintset_copy(struct m_smallintset* sis_src,
-                       struct m_smallintset* sis_dst);
-
+int m_smallintset_copy(struct m_smallintset* sis_src, struct m_smallintset* sis_dst);
 int m_smallintset_read(struct m_smallintset* sis, int fd);
 int m_smallintset_write(struct m_smallintset* sis, int fd);
-
 int m_smallintset_add(struct m_smallintset* sis, uint16_t value);
 int m_smallintset_add_all(struct m_smallintset* sis);
 int m_smallintset_remove(struct m_smallintset* sis, uint16_t value);
 int m_smallintset_remove_all(struct m_smallintset* sis);
 int m_smallintset_contains(struct m_smallintset* sis, uint16_t value);
-
-int m_smallintset_union(struct m_smallintset* sis_a,
-                        struct m_smallintset* sis_b);
-int m_smallintset_intersect(struct m_smallintset* sis_a,
-                            struct m_smallintset* sis_b);
+int m_smallintset_union(struct m_smallintset* sis_a, struct m_smallintset* sis_b);
+int m_smallintset_intersect(struct m_smallintset* sis_a, struct m_smallintset* sis_b);
 int m_smallintset_complement(struct m_smallintset* sis);
 
 #endif
