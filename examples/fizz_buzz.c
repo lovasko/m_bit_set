8fdf2564e689c1777db849f9d1afdab397f69754
Daniel Lovasko <lovasko@freebsd.org>
2016-02-17 18:01:53 +0100

    updated fizz buzz example

--- examples/fizz_buzz.c
+++ examples/fizz_buzz.c
@@ -2,38 +2,47 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include "../src/m_smallintset.h"
+#include <m_bit_set.h>
 
 int
 main(void)
 {
-	struct m_smallintset fizz;
-	struct m_smallintset buzz;
-	struct m_smallintset fizz_buzz;
-	uint16_t i;
+	struct m_bit_set fizz;
+	struct m_bit_set buzz;
+	struct m_bit_set fizz_buzz;
+	uint32_t i;
 
-	m_smallintset_init(&fizz, 65536, NULL);
-	m_smallintset_init(&buzz, 65536, NULL);
-	m_smallintset_init(&fizz_buzz, 65536, NULL);
+	m_bit_set_init(&fizz, 65536, NULL);
+	m_bit_set_init(&buzz, 65536, NULL);
+	m_bit_set_init(&fizz_buzz, 65536, NULL);
 
 	for (i = 0; i < 0xffff; i++)
 		if (i % 3 == 0)
-			m_smallintset_add(&fizz, i);
+			m_bit_set_add(&fizz, i);
 
 	for (i = 0; i < 0xffff; i++)
 		if (i % 5 == 0)
-			m_smallintset_add(&buzz, i);
+			m_bit_set_add(&buzz, i);
 
-	m_smallintset_copy(&fizz, &fizz_buzz);
-	m_smallintset_intersect(&fizz_buzz, &buzz);
+	m_bit_set_copy(&fizz_buzz, &fizz);
+	m_bit_set_intersect(&fizz_buzz, &buzz);
 
-	for (i = 0; i < 0xffff; i++)
-		if (m_smallintset_contains(&fizz_buzz, i) == M_SMALLINTSET_TRUE)
-			printf("Fizz buzz! %d\n", i);
+	for (i = 0; i < 0xff; i++) {
+		if (m_bit_set_test(&fizz_buzz, i) == M_BIT_SET_TRUE)
+			printf("Fizz buzz!");
+		else if (m_bit_set_test(&fizz, i) == M_BIT_SET_TRUE)
+			printf("Fizz!");
+		else if (m_bit_set_test(&buzz, i) == M_BIT_SET_TRUE)
+			printf("Buzz!");
+		else
+			printf("%d", i);
 
-	m_smallintset_free(&fizz);
-	m_smallintset_free(&buzz);
-	m_smallintset_free(&fizz_buzz);
+		printf("\n");
+	}
+
+	m_bit_set_free(&fizz);
+	m_bit_set_free(&buzz);
+	m_bit_set_free(&fizz_buzz);
 
 	return EXIT_SUCCESS;
 }
