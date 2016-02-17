eabec350342f8904391b94dde589e90d39f36d87
Daniel Lovasko <lovasko@freebsd.org>
2016-02-17 18:04:06 +0100

    read memory allocation

--- src/io.c
+++ src/io.c
@@ -1,4 +1,5 @@
 #include <sys/endian.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
 
@@ -22,9 +23,14 @@ m_bit_set_read(struct m_bit_set* bs, int fd)
 
 	bs->size = be32toh(size);
 	bs->max = be32toh(max);
+	bs->data = malloc(bs->size);
+	bs->own_memory = 1;
 
-	if (read(fd, bs->data, bs->size) != (ssize_t)bs->size)
+
+	if (read(fd, bs->data, bs->size) != (ssize_t)bs->size) {
+		free(bs->data);
 		return M_BIT_SET_E_IO;
+	}
 
 	return M_BIT_SET_OK;
 }
